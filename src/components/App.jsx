265b816f5d50ed0af8122467b35d7a0a389fb4ad
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2022-12-12 23:12:46 +0100

    add fourth component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import Statistics from './Statistics';
 import data from '../data/data.json';
 import FriendList from './FriendList'; 
 import friends from '../data/friends.json';
+import TransactionHistory from './TransactionHistory';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
@@ -17,6 +19,7 @@ export const App = () => {
       />
     <Statistics title="Upload stats" stats={data} />
     <FriendList friends={friends} />;
+    <TransactionHistory items={transactions} />;
     </div>
 
   );
